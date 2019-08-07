237b25cac82096f66ca8cdf21442d9ce5c3af16c
Rene Halver <r.halver@fz-juelich.de>
2019-08-07 14:04:18 -0600

    added computation of FFT on Cajita grid

--- CajitaFFTSolverFFTW.hpp
+++ CajitaFFTSolverFFTW.hpp
@@ -3,6 +3,8 @@
 
 #include "CajitaFFTSolver.hpp"
 
+#include "fftw3.h"
+
 template< class ExecutionSpace, class MemorySpace> class CajitaFFTSolverFFTW : 
     public CajitaFFTSolver< ExecutionSpace, MemorySpace > 
 {
@@ -45,9 +47,9 @@ template< class ExecutionSpace, class MemorySpace> class CajitaFFTSolverFFTW :
         virtual void q2grid( Kokkos::View<double*, MemorySpace>, 
                              Kokkos::View<double*, MemorySpace> );
 
-        virtual void forwardFFT() {}
+        virtual void forwardFFT() {};
 
-        virtual void backwardFFT() {}
+        virtual void backwardFFT();
 
         virtual void grid2E( Kokkos::View<double*, MemorySpace> ) {}
         virtual void grid2F( Kokkos::View<double*, MemorySpace> ) {}
@@ -85,5 +87,70 @@ void CajitaFFTSolverFFTW<ExecutionSpace, MemorySpace>::q2grid
     std::cout << "charges brought to grid" << std::endl;
 }
 
+template< class ExecutionSpace, class MemorySpace >
+void CajitaFFTSolverFFTW<ExecutionSpace, MemorySpace>::backwardFFT()
+{
+    // get private variables from the base class  
+    auto grid = this->getGrid();
+    auto layout = this->getLayout();
+    auto array = this->getArray();
+    auto gridDim = this->getGridDim();
+    // create get view and cellsize
+    auto view = array->view();
+    auto cellSize = grid->cellSize();
+    auto n_grid_points = gridDim(0) * gridDim(1) * gridDim(2);
 
+    // allocate FFTW data type arrays
+    fftw_complex* Qinput = new fftw_complex[n_grid_points];
+    fftw_complex* Qresult = new fftw_complex[n_grid_points];
+
+    // copy data to Qr array
+    Kokkos::parallel_for
+    (
+        n_grid_points,
+        KOKKOS_LAMBDA(int idx)
+        {
+            int cz = idx % gridDim(2);
+            int cy = ( idx / gridDim(2) ) % gridDim(1);
+            int cx = idx / ( gridDim(1) * gridDim(2) );
+
+            Qinput[idx][0] = view(cx,cy,cz,0);
+            Qinput[idx][1] = 0.0;
+        }
+    );
+
+    // create FFTW plan
+    fftw_plan plan;
+
+    plan = fftw_plan_dft_3d((int)view.extent(0), 
+                            (int)view.extent(1), 
+                            (int)view.extent(2),
+                            Qinput,
+                            Qresult,
+                            FFTW_BACKWARD,
+                            FFTW_ESTIMATE);
+
+   
+    // execute FFTW plan
+    fftw_execute(plan);
+    
+    
+    // copy results to grid
+    Kokkos::parallel_for
+    (
+        n_grid_points,
+        KOKKOS_LAMBDA(int idx)
+        {
+            int cz = idx % gridDim(2);
+            int cy = ( idx / gridDim(2) ) % gridDim(1);
+            int cx = idx / ( gridDim(1) * gridDim(2) );
+
+            view(cx,cy,cz,1) = Qresult[idx][0];
+            view(cx,cy,cz,2) = Qresult[idx][1];
+        }
+    );
+
+    delete [] Qresult;
+    delete [] Qinput;
+}
 #endif
